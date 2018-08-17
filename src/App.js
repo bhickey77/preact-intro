0d782762c0afda430ced1dcb4d2501b4ef8c24dc
Bill Hickey <bhickey77@gmail.com>
2018-08-16 22:20:46 -0500

    finish intro

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import { h, Component } from 'preact';
 import logo from './logo.svg';
 import './App.css';
+import linkState from 'linkstate';
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      nameList: [],
+    }
+  }  
+
+  addName = () => {
+    this.setState({
+      ...this.state,
+      nameList: [
+        ...this.state.nameList,
+        this.state.name,
+      ],
+      name: '',
+    })
+  }
+  
+  render({}, {name, nameList}) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div class="App">
+        <header class="App-header">
+          <img src={logo} class="App-logo" alt="logo" />
+          <h1 class="App-title">Welcome to Preact</h1>
         </header>
-        <p className="App-intro">
+        <p class="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="text" value={name} onInput={linkState(this, 'name')}/>
+        <input type="submit" onClick={this.addName}/>
+        <ul>
+          {
+            nameList.map(name => { return <li> {name} </li>})
+          }
+        </ul>
       </div>
     );
   }
